ff1dc354840a73ad5dd9fc3c68b0ddf913fa25cc
Robert Wilcox <stampylongr7@gmail.com>
2021-03-10 16:11:57 +0000

    AAAAA

---
+++ src/components/Text.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-auto text-center space-y-5">
+    <h1 class="text-gray-900 text-5xl dark:text-white">
+      the mint farm is down!
+    </h1>
+    <h3 class="text-gray-600 text-2xl dark:text-gray-300 pb-8">
+      the mint farm and all its services are currently down. we are migrating
+      isps at the moment, so we may be down for a few days. if you need to view
+      the mastodon to review adding it to the list, please click the button
+      below
+    </h3>
+    <a
+      href="/snapshots"
+      class="p-3 btn-indigo transform hover:scale-110 motion-reduce:transform-none"
+    >
+      snapshots of the instance
+    </a>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      padding: ["hover"],
+      translate: ["motion-reduce"],
+    },
   },
   plugins: [],
 };
